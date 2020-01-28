9579989d00b4221aa4538e5def86f076f894f312
Alisson Youssf <youssfbr@gmail.com>
2020-01-28 19:12:39 -0300

    Criado Componente painel-negociacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PainelNegociacaoComponent } from './painel-negociacao/painel-negociacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelNegociacaoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/painel-negociacao/painel-negociacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-painel-negociacao',
+  templateUrl: './painel-negociacao.component.html',
+  styleUrls: ['./painel-negociacao.component.css']
+})
+export class PainelNegociacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
