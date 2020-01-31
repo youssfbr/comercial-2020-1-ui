0946ff38571cbb3f1c04e1ae66357b62de2e47f4
Alisson Youssf <youssfbr@gmail.com>
2020-01-31 00:17:07 -0300

    Criado componente painel-negociacao

--- src/app/painel-negociacao/painel-negociacao.component.ts
+++ src/app/painel-negociacao/painel-negociacao.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelNegociacaoComponent implements OnInit {
 
+  oportunidades = [
+    { descricao: 'Projeto de dev', nomeProspecto: 'Ali Baba', valor: 50000 },
+    { descricao: 'Projeto de dev1', nomeProspecto: 'Ali Baba1', valor: 60000 }
+  ];
+
   constructor() { }
 
   ngOnInit() {
